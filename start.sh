d5bef68424f0761bb65da5b241377934bee46168
Asanga Udara <84509909+asangabro@users.noreply.github.com>
2021-10-13 14:59:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/asangabro/lol.git /lol
-cd /lol
-pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
